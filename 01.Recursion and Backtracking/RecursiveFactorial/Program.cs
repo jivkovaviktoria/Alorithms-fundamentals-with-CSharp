dc17c093884a2c31962bf5a7e3db8cce5b735bf2
Viktoriya Jivkova <Jivkovaviktoria@gmail.com>
2022-06-17 13:47:36 +0300

    Solved task "Recursive factorial" from Recursion and backtracking-lab

---
+++ 01.Recursion and Backtracking/RecursiveFactorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetFactorial(number));
+        }
+
+        private static int GetFactorial(int number)
+        {
+            if (number == 0)
+                return 1;
+
+            return number * GetFactorial(number - 1);
+
+        }
+    }
+}
